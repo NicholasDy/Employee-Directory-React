6d8086f2d9c4d563d506d98b2a68a726e7eb70f3
NicholasDy <77211265+NicholasDy@users.noreply.github.com>
2021-06-12 19:45:49 -0400

    added sorting to each of the columns

--- employee-directory-react/src/components/Table/Table.js
+++ employee-directory-react/src/components/Table/Table.js
@@ -1,4 +1,4 @@
-import { useTable } from 'react-table'
+import { useTable, useSortBy } from 'react-table'
 
 
 function Table({ columns, data }) {
@@ -10,33 +10,44 @@ function Table({ columns, data }) {
         headerGroups,
         rows,
         prepareRow,
-    } = useTable({ columns, data })
-    
-  return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-              })}
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
-  )
+    } = useTable(
+        { columns, data },
+        useSortBy
+    )
+
+    return (
+        <table {...getTableProps()} class="table table-striped table-dark" id="sortTable">
+            <thead class="thead-light">
+                {headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()} scope="col">
+                        {headerGroup.headers.map(column => (
+                            <th {...column.getHeaderProps(column.getSortByToggleProps())}>{column.render('Header')}
+                                <span>
+                                    {column.isSorted
+                                        ? column.isSortedDesc
+                                            ? ' 🔽'
+                                            : ' 🔼'
+                                        : ''}
+                                </span>
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row, i) => {
+                    prepareRow(row)
+                    return (
+                        <tr {...row.getRowProps()} >
+                            {row.cells.map(cell => {
+                                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                            })}
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    )
 }
 
 
--- employee-directory-react/src/utils/API.js
+++ employee-directory-react/src/utils/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default{
     getRandomPeople: function(){
-        return axios.get("https://randomuser.me/api/?results=100&inc=name,email,gender,registered,cell")
+        return axios.get("https://randomuser.me/api/?results=20&inc=name,email,gender,registered,cell")
     }
 };
