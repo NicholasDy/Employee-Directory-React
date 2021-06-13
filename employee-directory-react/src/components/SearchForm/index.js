1998b36465243007cc7b95106613a5d005680acd
NicholasDy <77211265+NicholasDy@users.noreply.github.com>
2021-06-12 21:09:35 -0400

    adding styling and final filtering of the app

--- employee-directory-react/src/components/SearchForm/index.js
+++ employee-directory-react/src/components/SearchForm/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const GlobalFilter = ({ filter, setFilter }) => {
   return (
-    <span>
-      Search: {' '}
-      <input value={filter || ""} onChange={e => setFilter(e.target.value)} />
+    <span class="d-flex justify-content-center p-3">
+      Search Any Column: {' '}
+      <input value={filter || ""} onChange={e => setFilter(e.target.value)} class="ms-1" />
     </span>
   )
 }
